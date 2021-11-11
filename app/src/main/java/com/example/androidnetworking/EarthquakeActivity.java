155472916e2bd889d31ed4907bc796677e434858
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-11-11 21:44:30 +0100

    Modified and added Intent for URL

--- app/src/main/java/com/example/androidnetworking/EarthquakeActivity.java
+++ app/src/main/java/com/example/androidnetworking/EarthquakeActivity.java
@@ -2,7 +2,11 @@ package com.example.androidnetworking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,7 +24,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         ArrayList<EarthquakeData> earthquakes = QueryUtils.extractEarthquakes();
 
         // Create a new {@link EarthquakeAdapter} of earthquakes
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this,  earthquakes);
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this,  earthquakes);
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
@@ -28,5 +32,16 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener((parent, view, position, id) -> {
+            // Find the current earthquake that was clicked on
+            EarthquakeData currentData = adapter.getItem(position);
+
+            // Convert the String URL into a URI object (to pass into the Intent constructor)
+            Uri earthquakeUri = Uri.parse(currentData.getmUrl());
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+            startActivity(intent);
+        });
     }
 }
